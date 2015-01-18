b3a6f23579c818a70660dba51b99c6357093ee5f
Javivi <Javivi@Javivi-ASUS>
2015-01-18 13:18:15 +0000

    UI implemented

---
+++ src/ui/Resizer.java
@@ -0,0 +1,39 @@
+package ui;
+
+public class Resizer {
+
+    private final int width, height;
+
+    public Resizer(int width, int height) {
+        this.width = width;
+        this.height = height;
+    }
+
+    public Size resize(int width, int height) {
+        double wRatio = 1 * this.width / width;
+        double hRatio = 1 * this.height / height;
+        return hRatio < wRatio
+                ? new Size((int) (width * hRatio), (int) (height * hRatio))
+                : new Size((int) (width * wRatio), (int) (height * wRatio));
+    }
+
+    public class Size {
+
+        private int width, height;
+
+        private Size(int width, int height) {
+            this.width = width;
+            this.height = height;
+        }
+
+        public int getWidth() {
+            return width;
+        }
+
+        public int getHeight() {
+            return height;
+        }
+
+    }
+
+}
