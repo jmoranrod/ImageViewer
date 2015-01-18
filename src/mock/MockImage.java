ad47576583f8db7f0b05bf0c67b236ecb6248eaa
Javivi <Javivi@Javivi-ASUS>
2015-01-18 12:04:22 +0000

    MockImage created

---
+++ src/mock/MockImage.java
@@ -0,0 +1,35 @@
+package mock;
+
+import model.Bitmap;
+import model.Image;
+
+public class MockImage implements Image {
+
+    @Override
+    public Image getNext() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Image getPrev() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void setNext(Image image) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void setPrev(Image image) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Bitmap getBitmap() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    
+    
+}
